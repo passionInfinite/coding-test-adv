a96a924b8fbd3e9f625a552b97d198872f4193a2
Hugh Soong <hugh.soong@rbc.com>
2021-10-08 17:23:33 -0400

    refactor: cleanup

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const createError = require('http-errors');
 const express = require('express');
-const morgan = require('morgan');
+const expressWinston = require('express-winston');
 // const path = require('path');
 const cookieParser = require('cookie-parser');
 
@@ -9,7 +8,7 @@ const authRouter = require('./routes/auth');
 const createApp = (logger) => {
   const app = express();
 
-  app.use(morgan('dev'));
+  app.use(expressWinston.logger({ winstonInstance: logger }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
@@ -21,18 +20,12 @@ const createApp = (logger) => {
   app.use('/auth', authRouter);
 
   // catch 404 and forward to error handler
-  app.use((req, res, next) => {
-    next(createError(404));
+  app.use((req, res) => {
+    res.status(404).send('Not found');
   });
 
   // error handler
   app.use((err, req, res) => {
-  // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    logger.error(err);
     res.status(err.status || 500);
   });
 
